3d8efa5a6ba7d1a3040fca1b36893e971f62d5f1
Harsh Yadav <hy945196@gmail.com>
2023-10-15 07:50:41 +0530

    Added all the functionality

--- expenses/Component/Form.js
+++ expenses/Component/Form.js
@@ -1,19 +1,36 @@
 import React, { useRef } from 'react'
 
-const form = ({Income,setIncome}) => {
+const form = ({income,setIncome}) => {
   const desc = useRef(null) 
   const price = useRef(null) 
   const date = useRef(null) 
-  const AddIncome = (e)=>{
-    e.preventDefault()
-    console.log(desc.current.value)
-    console.log(price.current.value)
-    console.log(date.current.value)
+  // const AddIncome = (e)=>{
+  //   e.preventDefault()
+  //   console.log(desc.current.value)
+  //   console.log(price.current.value)
+  //   console.log(date.current.value)
 
-    desc.current.value="";
-    price.current.value="";
-    date.current.value="";
-  }
+    
+
+    const AddIncome= e =>{
+      e.preventDefault();
+      let d = desc.current.value.split("-");
+      // let newDate=new Date[d[0],d[1]-1,d[2]];
+
+      setIncome([...income,{
+        "desc": desc.current.value,
+        "date": date.current.value,
+        "price":price.current.value
+      }]);
+
+      desc.current.value="";
+      price.current.value=null;
+      date.current.value=null;
+      
+    }
+      
+
+  
 
   return (
     <form className='income-form' onSubmit={AddIncome}>
--- expenses/app/page.js
+++ expenses/app/page.js
@@ -3,24 +3,60 @@ import React, { useEffect, useState } from "react";
 import Header from "@/Component/Header";
 import Form from "@/Component/Form";
 const page=()=>{
-    const [Income, setIncome] = useState("")
+    const [income, setIncome] = useState([])
     const [totalIncome, settotalIncome] = useState(0);
+
+
+    const deleteNote=(i)=>{
+      let copyTask=[...income];
+      copyTask.splice(i,1)
+      setIncome(copyTask);
+ }
+
+    let renderTask=<h5>No Task Available</h5>
+
+    if(income.length > 0){
+      renderTask=income.map((t,i)=>{
+        return(
+          <>
+            <li className="income-list">
+               <div key={i} className="income-list-1">
+                 <h5 className="description">{t.desc}</h5>
+                 <p className="text-xl ">{t.price}</p>
+                 <p className="text-xl ">{t.date}</p>
+               </div> 
+               <button
+                 onClick={()=>{
+                  deleteNote(i);
+                 }}
+                  className="income-list-2">Delete</button>
+            </li> 
+          </>  
+        )
+      })
+    }
+  
     
-    useEffect(()=>{
-      let temp=0;
-      for(let i=0;i<Income.length;i++)
-      {
-        temp+=parseInt(Income[i].price)
+    useEffect(() => {
+      let temp = 0;
+      for(let i = 0; i < income.length; i++) {
+        temp += parseInt(income[i].price);
       }
+  
       settotalIncome(temp);
-    },[Income]);
+    }, [income]);
 
   return(
     <>
     <Header totalIncome={totalIncome}/>
-    <Form Income={Income} setIncome={setIncome}/>
+    <Form income={income} setIncome={setIncome}/>
+    {/* <div className="w-2/3 h-15 text-black">{(income.length > 0) ? income[0].desc : " "}</div> */}
+    <hr/>
+    <div>
+       <ul>{renderTask}</ul>
+    </div>
     </>
-  )
+  );
 }
 
 export default page
